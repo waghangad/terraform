3791047c48a310e93af010e3bb87b63fd4539615
Angad Wagh <awsclassangad@gmail.com>
2022-03-12 10:30:33 +0530

    awsclassangad added subnets

--- study-template.tf
+++ study-template.tf
@@ -17,3 +17,11 @@ provider "aws" {
 resource "aws_vpc" "study-vpc" {
   cidr_block = "10.0.0.0/16"
 }
+# Create a Subnets
+resource "aws_subnet" "study-subnet" {
+  vpc_id = aws_vpc.study-vpc.id
+  cidr_block = "10.0.1.0/24"
+  tags = {
+	Name = "pri-sbnt-1a"
+  }
+}
